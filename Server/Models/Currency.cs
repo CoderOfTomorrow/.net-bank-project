d8c21164527511efc11d3e32ec3814592bb9b155
blackb0y999 <blackb0y999@yahoo.com>
2020-11-17 10:28:00 +0200

    //Added currency //Add currency convertor if source currency and destiantion currency are different

---
+++ Server/Models/Currency.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Endava_Project.Server.Models
+{
+    public class Currency
+    {
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+    }
+}
